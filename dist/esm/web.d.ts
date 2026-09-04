45196e113869b3103f0ca5bcbe212afb8a6e5fb2
den <trinitiwowka@gmail.com>
2026-09-04 09:26:49 +0200

    build: package capacitor 7 deferred link fix

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,25 @@
+import { WebPlugin } from '@capacitor/core';
+import type { FacebookLoginPlugin, FacebookLoginResponse, FacebookCurrentAccessTokenResponse, FacebookDeferredDeepLinkResponse, FacebookConfiguration } from './definitions';
+declare global {
+    interface Window {
+        fbAsyncInit: () => void;
+    }
+}
+export declare class FacebookLoginWeb extends WebPlugin implements FacebookLoginPlugin {
+    initialize(options: Partial<FacebookConfiguration>): Promise<void>;
+    private loadScript;
+    login(options: {
+        permissions: string[];
+    }): Promise<FacebookLoginResponse>;
+    logout(): Promise<void>;
+    reauthorize(): Promise<FacebookLoginResponse>;
+    getCurrentAccessToken(): Promise<FacebookCurrentAccessTokenResponse>;
+    getProfile<T extends Record<string, unknown>>(options: {
+        fields: readonly string[];
+    }): Promise<T>;
+    logEvent(): Promise<void>;
+    setAutoLogAppEventsEnabled(): Promise<void>;
+    setAdvertiserTrackingEnabled(): Promise<void>;
+    setAdvertiserIDCollectionEnabled(): Promise<void>;
+    getDeferredDeepLink(): Promise<FacebookDeferredDeepLinkResponse>;
+}
